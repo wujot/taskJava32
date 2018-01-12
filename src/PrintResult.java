7e6f740be3762a967fed2b0fce23994fbe9016d8
wujot <wojciech.wujot"gmail.com>
2018-01-12 12:18:34 +0100

    Printed task document with answers.

--- src/PrintResult.java
+++ src/PrintResult.java
@@ -4,18 +4,19 @@ public class PrintResult {
     public static void main (String[] args) {
 
         // print result
-        System.out.println("Klasa to …");
-        System.out.println("Obiekt to … i jest tworzony na podstawie …");
-        System.out.println("Zmienna to …");
-        System.out.println("Kazda zmienna musi miec okreslony …, np. int, albo String.");
-        System.out.println("W klasie moga się znajdowac …, … i …");
-        System.out.println("… sa wywolywane przez operator … i sluza do tworzenia nowych obiektow.");
-        System.out.println("Jesli w klasie zdefiniowano chociaz jeden … to kompilator nie wygeneruje dla nas … domyslnego.");
+        System.out.println("Klasa to zbior obiektow.");
+        System.out.println("Obiekt to zmienna i jest tworzony na podstawie konstruktora");
+        System.out.println("Zmienna to typ danych");
+        System.out.println("Kazda zmienna musi miec okreslony typ, np. int, albo String.");
+        System.out.println("W klasie moga się znajdowac metody, obiekty i zmienne.");
+        System.out.println("Metody sa wywolywane przez operator zmiennej i sluza do tworzenia nowych obiektow.");
+        System.out.println("Jesli w klasie zdefiniowano chociaz jeden obiekt to kompilator nie wygeneruje dla nas " +
+                "konstruktora domyslnego.");
         System.out.println("Zakladajac, ze istnieje klasa Car, to przy zapisie:\n");
         System.out.println("Car car1 = new Car(„BMW”, „M5”, 2007, 2.6);\n");
-        System.out.println("car1 nazywamy …,\n");
+        System.out.println("car1 nazywamy zmienna,\n");
         System.out.println("a przy zapisie:\n");
         System.out.println("Car car1 = new Car(„BMW”, „M5”, 2007, 2.6);  Car car2 = car1;\n");
-        System.out.println("car1 I car2 to dwie … wskazujace na ten sam …\n");
+        System.out.println("car1 I car2 to dwie zmienne wskazujace na ten sam obiekt\n");
     }
 }
