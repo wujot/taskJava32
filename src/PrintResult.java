5ef91b951d9ecc5914fce890cb48beb69b5d2f77
wujot <wojciech.wujot"gmail.com>
2018-01-12 12:11:19 +0100

    Plain task document.

---
+++ src/PrintResult.java
@@ -0,0 +1,21 @@
+public class PrintResult {
+
+    // main method
+    public static void main (String[] args) {
+
+        // print result
+        System.out.println("Klasa to …");
+        System.out.println("Obiekt to … i jest tworzony na podstawie …");
+        System.out.println("Zmienna to …");
+        System.out.println("Kazda zmienna musi miec okreslony …, np. int, albo String.");
+        System.out.println("W klasie moga się znajdowac …, … i …");
+        System.out.println("… sa wywolywane przez operator … i sluza do tworzenia nowych obiektow.");
+        System.out.println("Jesli w klasie zdefiniowano chociaz jeden … to kompilator nie wygeneruje dla nas … domyslnego.");
+        System.out.println("Zakladajac, ze istnieje klasa Car, to przy zapisie:\n");
+        System.out.println("Car car1 = new Car(„BMW”, „M5”, 2007, 2.6);\n");
+        System.out.println("car1 nazywamy …,\n");
+        System.out.println("a przy zapisie:\n");
+        System.out.println("Car car1 = new Car(„BMW”, „M5”, 2007, 2.6);  Car car2 = car1;\n");
+        System.out.println("car1 I car2 to dwie … wskazujace na ten sam …\n");
+    }
+}
